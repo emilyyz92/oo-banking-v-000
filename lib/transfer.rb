cda87880d86e6f7a167a84a7dccfada0e4402e7f
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-21 20:14:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -6,7 +6,7 @@ attr_accessor :sender, :receiver, :status, :amount
   def initialize(sender,receiver,amount)
     @sender = sender
     @receiver = receiver
-    @status = pending
+    @status = "pending"
     @amount = amount
 
   end
