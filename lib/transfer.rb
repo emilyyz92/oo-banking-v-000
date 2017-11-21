d17578f4274acece21ef4bf62a2c2a1af12309c1
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-21 20:04:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,19 @@
 class Transfer
 attr_accessor :sender, :receiver, :status, :amount
+
+@@all = []
+
   def initialize(sender,receiver,amount)
     @sender = sender
     @receiver = receiver
     @status = pending
     @amount = amount
   end
+
+  def valid?
+    if BankAccount.all.include?(self)
+      return true
+    elsif 
+      
+      
 end
